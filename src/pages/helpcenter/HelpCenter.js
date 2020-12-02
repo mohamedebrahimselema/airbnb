594b18041ac290773c357b4c901fa18488c2a347
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-12-02 23:10:15 +0200

    --

---
+++ src/pages/helpcenter/HelpCenter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./HelpCenter.css";
+
+const HelpCenter = () => {
+  return (
+    <body>
+      <div>
+        <div className="_hgs47m">
+          <div className="_n5lh69r">
+            <div className="margin-container">
+              <h1 tabindex="-1" className="_14i3z6h">
+                <div className="_1yrl4d7p">
+                  <div id="FMP-target" className="_11ry7lz">
+                    Hi, what can we help you with today?
+                  </div>
+                </div>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
+
+export default HelpCenter;
